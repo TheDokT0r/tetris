a3cc3243b0007aff6eb2bfa279f34469bf63099f
Or Kwitzel <orkv1234@gmail.com>
2026-02-02 19:50:06 +0200

    fixed prettierrc and added svelte support

--- src/compnents/Game.svelte
+++ src/compnents/Game.svelte
@@ -1,104 +1,99 @@
 <script lang="ts">
-    import { onMount } from "svelte";
-    import "@/style.css";
-    import Game, { Movement } from "@/gameObj/game";
-    import { type Block } from "@/gameObj/tetromino";
+  import { onMount } from "svelte";
+  import "@/style.css";
+  import Game, { Movement } from "@/gameObj/game";
+  import { type Block } from "@/gameObj/tetromino";
 
-    let ctx = $state<CanvasRenderingContext2D | null>(null);
-    let canvas = $state<HTMLCanvasElement | null>();
-    let gameSpeedMs = 1000;
-    const BLOCK_SIZE = 50 as const;
-    const game = new Game(10, 20);
+  let ctx = $state<CanvasRenderingContext2D | null>(null);
+  let canvas = $state<HTMLCanvasElement | null>();
+  let gameSpeedMs = 1000;
+  const BLOCK_SIZE = 50 as const;
+  const game = new Game(10, 20);
 
-    function setCanvasSize() {
-        if (!canvas) return;
-        const { board } = game;
-        canvas.height = board.length * BLOCK_SIZE;
-        canvas.width = board[0].length * BLOCK_SIZE;
+  function setCanvasSize() {
+    if (!canvas) return;
+    const { board } = game;
+    canvas.height = board.length * BLOCK_SIZE;
+    canvas.width = board[0].length * BLOCK_SIZE;
+  }
+
+  function drawBlock(block: Block, x: number, y: number) {
+    if (!ctx || !block) return;
+
+    const absoluteX = BLOCK_SIZE * x;
+    const absoluteY = BLOCK_SIZE * y;
+
+    ctx.save();
+
+    ctx.fillStyle = block.color;
+    ctx.fillRect(absoluteX, absoluteY, BLOCK_SIZE, BLOCK_SIZE);
+
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 1;
+    ctx.strokeRect(absoluteX + 0.5, absoluteY + 0.5, BLOCK_SIZE - 1, BLOCK_SIZE - 1);
+
+    ctx.restore();
+  }
+
+  function drawBoard() {
+    const { board } = game;
+    for (let i = 0; i < board.length; i++) {
+      for (let j = 0; j < board[0].length; j++) {
+        drawBlock(board[i][j], j, i);
+      }
     }
+  }
 
-    function drawBlock(block: Block, x: number, y: number) {
-        if (!ctx || !block) return;
-
-        const absoluteX = BLOCK_SIZE * x;
-        const absoluteY = BLOCK_SIZE * y;
-
-        ctx.save();
-
-        ctx.fillStyle = block.color;
-        ctx.fillRect(absoluteX, absoluteY, BLOCK_SIZE, BLOCK_SIZE);
-
-        ctx.strokeStyle = "black";
-        ctx.lineWidth = 1;
-        ctx.strokeRect(
-            absoluteX + 0.5,
-            absoluteY + 0.5,
-            BLOCK_SIZE - 1,
-            BLOCK_SIZE - 1,
-        );
-
-        ctx.restore();
+  function drawTetromino() {
+    const { playedPiece } = game;
+    const { blocks, position } = playedPiece;
+    for (let i = 0; i < blocks.length; i++) {
+      for (let j = 0; j < blocks[0].length; j++) {
+        drawBlock(blocks[i][j], position.x + j, position.y + i);
+      }
     }
+  }
 
-    function drawBoard() {
-        const { board } = game;
-        for (let i = 0; i < board.length; i++) {
-            for (let j = 0; j < board[0].length; j++) {
-                drawBlock(board[i][j], j, i);
-            }
-        }
-    }
+  function redraw() {
+    if (!ctx || !canvas) return;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBoard();
+    drawTetromino();
+  }
 
-    function drawTetromino() {
-        const { playedPiece } = game;
-        const { blocks, position } = playedPiece;
-        for (let i = 0; i < blocks.length; i++) {
-            for (let j = 0; j < blocks[0].length; j++) {
-                drawBlock(blocks[i][j], position.x + j, position.y + i);
-            }
-        }
-    }
+  export function initGameCanvas() {
+    if (!canvas) return;
+    ctx = canvas.getContext("2d");
+    setCanvasSize();
+    redraw();
 
-    function redraw() {
-        if (!ctx || !canvas) return;
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        drawBoard();
-        drawTetromino();
-    }
+    document.addEventListener("keydown", (event) => {
+      if (event.key == "ArrowRight") {
+        game.movePiece(Movement.RIGHT);
+      } else if (event.key == "ArrowLeft") {
+        game.movePiece(Movement.LEFT);
+      } else if (event.key == "ArrowUp") {
+        game.movePiece(Movement.ROTATE);
+      } else if (event.key == "ArrowDown") {
+        game.movePiece(Movement.DOWN);
+      } else if (event.key == "r") {
+        game.quickDrop();
+      } else if (event.key == "q") {
+        game.swapPiece();
+      }
 
-    export function initGameCanvas() {
-        if (!canvas) return;
-        ctx = canvas.getContext("2d");
-        setCanvasSize();
-        redraw();
-
-        document.addEventListener("keydown", (event) => {
-            if (event.key == "ArrowRight") {
-                game.movePiece(Movement.RIGHT);
-            } else if (event.key == "ArrowLeft") {
-                game.movePiece(Movement.LEFT);
-            } else if (event.key == "ArrowUp") {
-                game.movePiece(Movement.ROTATE);
-            } else if (event.key == "ArrowDown") {
-                game.movePiece(Movement.DOWN);
-            } else if (event.key == "r") {
-                game.quickDrop();
-            } else if (event.key == "q") {
-                game.swapPiece();
-            }
-
-            redraw();
-        });
-
-        setInterval(() => {
-            game.movePiece(Movement.DOWN);
-            redraw();
-        }, gameSpeedMs);
-    }
-
-    onMount(() => {
-        initGameCanvas();
+      redraw();
     });
+
+    setInterval(() => {
+      game.movePiece(Movement.DOWN);
+      redraw();
+    }, gameSpeedMs);
+  }
+
+  onMount(() => {
+    initGameCanvas();
+  });
 </script>
 
 <canvas bind:this={canvas}></canvas>
--- src/compnents/SavedPiece.svelte
+++ src/compnents/SavedPiece.svelte
@@ -1,61 +1,56 @@
 <script lang="ts">
-    import { savedPiece } from "@/stores/gameData";
+  import { savedPiece } from "@/stores/gameData";
   import { onMount } from "svelte";
-    const BLOCK_SIZE = 50 as const;
+  const BLOCK_SIZE = 50 as const;
 
-    let canvas = $state<HTMLCanvasElement>();
-    let ctx = $state<CanvasRenderingContext2D | null>(null);
+  let canvas = $state<HTMLCanvasElement>();
+  let ctx = $state<CanvasRenderingContext2D | null>(null);
 
-    onMount(() => {
-      if (!canvas) return;
-      ctx = canvas.getContext("2d");
-    })
+  onMount(() => {
+    if (!canvas) return;
+    ctx = canvas.getContext("2d");
+  });
 
-    function drawSavedPiece() {
-        if (!canvas) return;
-        ctx = canvas.getContext("2d");
-        if (!ctx) return;
+  function drawSavedPiece() {
+    if (!canvas) return;
+    ctx = canvas.getContext("2d");
+    if (!ctx) return;
 
-        if (!$savedPiece.piece) return;
+    if (!$savedPiece.piece) return;
 
-        const { blocks } = $savedPiece.piece;
-        for (let y = 0; y < blocks.length; y++) {
-            for (let x = 0; x < blocks[0].length; x++) {
-                const block = blocks[y][x];
-                if (block) {
-                    const absoluteX = BLOCK_SIZE * x;
-                    const absoluteY = BLOCK_SIZE * y;
-                    ctx.save();
-                    ctx.fillStyle = block.color;
-                    ctx.fillRect(absoluteX, absoluteY, BLOCK_SIZE, BLOCK_SIZE);
-                    ctx.strokeStyle = "black";
-                    ctx.lineWidth = 1;
-                    ctx.strokeRect(
-                        absoluteX + 0.5,
-                        absoluteY + 0.5,
-                        BLOCK_SIZE - 1,
-                        BLOCK_SIZE - 1,
-                    );
+    const { blocks } = $savedPiece.piece;
+    for (let y = 0; y < blocks.length; y++) {
+      for (let x = 0; x < blocks[0].length; x++) {
+        const block = blocks[y][x];
+        if (block) {
+          const absoluteX = BLOCK_SIZE * x;
+          const absoluteY = BLOCK_SIZE * y;
+          ctx.save();
+          ctx.fillStyle = block.color;
+          ctx.fillRect(absoluteX, absoluteY, BLOCK_SIZE, BLOCK_SIZE);
+          ctx.strokeStyle = "black";
+          ctx.lineWidth = 1;
+          ctx.strokeRect(absoluteX + 0.5, absoluteY + 0.5, BLOCK_SIZE - 1, BLOCK_SIZE - 1);
 
-                    ctx.restore();
-                }
-            }
+          ctx.restore();
         }
+      }
     }
+  }
 
-    $effect(() => {
-        if (!$savedPiece.piece || !canvas || !ctx) return;
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+  $effect(() => {
+    if (!$savedPiece.piece || !canvas || !ctx) return;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-        drawSavedPiece();
-    });
+    drawSavedPiece();
+  });
 </script>
 
 <div>
-    <canvas bind:this={canvas}></canvas>
-    {#if $savedPiece.piece}
-        <p>Holding: {$savedPiece.piece.id}</p>
-    {:else}
-        <p>Holding: Nothing</p>
-    {/if}
+  <canvas bind:this={canvas}></canvas>
+  {#if $savedPiece.piece}
+    <p>Holding: {$savedPiece.piece.id}</p>
+  {:else}
+    <p>Holding: Nothing</p>
+  {/if}
 </div>
