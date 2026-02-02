7200a91faf4aece8368901167ef5aa57b3b4d94f
Or Kwitzel <orkv1234@gmail.com>
2026-02-02 10:20:25 +0200

    create basic saved piece component (need reworking

--- src/compnents/SavedPiece.svelte
+++ src/compnents/SavedPiece.svelte
@@ -1,9 +1,50 @@
 <script lang="ts">
     import { savedPiece } from "@/stores/gameData";
+    import { onMount } from "svelte";
+    const BLOCK_SIZE = 50 as const;
+
+    let canvas = $state<HTMLCanvasElement>();
+
+    function drawSavedPiece() {
+        if (!canvas) return;
+        const ctx = canvas.getContext("2d");
+        if (!ctx) return;
+
+        if (!$savedPiece.piece) return;
+
+        const { blocks } = $savedPiece.piece;
+        for (let y = 0; y < blocks.length; y++) {
+            for (let x = 0; x < blocks[0].length; x++) {
+                const block = blocks[y][x];
+                if (block) {
+                    const absoluteX = BLOCK_SIZE * x;
+                    const absoluteY = BLOCK_SIZE * y;
+                    ctx.save();
+                    ctx.fillStyle = block.color;
+                    ctx.fillRect(absoluteX, absoluteY, BLOCK_SIZE, BLOCK_SIZE);
+                    ctx.strokeStyle = "black";
+                    ctx.lineWidth = 1;
+                    ctx.strokeRect(
+                        absoluteX + 0.5,
+                        absoluteY + 0.5,
+                        BLOCK_SIZE - 1,
+                        BLOCK_SIZE - 1,
+                    );
+
+                    ctx.restore();
+                }
+            }
+        }
+    }
+
+    $effect(() => {
+        if (!$savedPiece.piece) return;
+        drawSavedPiece();
+    });
 </script>
 
 <div>
-    <canvas></canvas>
+    <canvas bind:this={canvas}></canvas>
     {#if $savedPiece.piece}
         <p>Holding: {$savedPiece.piece.id}</p>
     {:else}
